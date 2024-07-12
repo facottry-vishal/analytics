701b9dcd690e73f32fca89274dcd47d08d5d5d60
Styles <facottry.dev@gmail.com>
2024-07-12 18:02:24 +0530

    build error fix

--- client/src/app/sample/page.jsx
+++ client/src/app/sample/page.jsx
@@ -2,10 +2,10 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Navbar from "../../components/Navbar";
-import Pricing from "../../components/Pricing";
-import Footer from "../../components/Footer";
-import Hero from "../../components/Hero";
+import Navbar from "@/Components/Navbar";
+import Hero from "@/Components/Hero";
+import Pricing from "@/Components/Pricing";
+import Footer from "@/Components/Footer";
 
 export default function Sample() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
