bec92b74277489b13490863266c13429df56e4fa
Styles <facottry.dev@gmail.com>
2024-07-19 00:47:44 +0530

    fixed getCount

--- server/controllers/b2b.js
+++ server/controllers/b2b.js
@@ -1,42 +1,15 @@
 import logs from "../models/logs.js";
-import count from "../models/count.js";
 
 export const updateLogs = async (req, res) => {
   try {
     const { projectID, filter, pathname } = req.body;
 
-    // UPDATE COUNT COLLECTION
-    const countDoc = await count.findOne({ projectID });
-    if (countDoc) {
-      const filterCountObj = countDoc.count;
-
-      Object.values(filter).forEach((key) => {
-        if (filterCountObj[key]) {
-          filterCountObj[key] += 1;
-        } else {
-          filterCountObj[key] = 1;
-        }
-      });
-      
-      countDoc.markModified("count");
-      await countDoc.save();
-    } else {
-      const filterCountObj = {};
-
-      Object.values(filter).forEach((key) => {
-        filterCountObj[key] = 1;
-      });
-
-      const newDoc = new count({ projectID, count: filterCountObj });
-      await newDoc.save();
-    }
-
-    // UPDATE LOG COLLECTION
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     const tomorrow = new Date(today);
     tomorrow.setDate(today.getDate() + 1);
 
+    // UPDATE LOG COLLECTION
     const log = await logs.findOne({
       projectID,
       filter,
@@ -45,12 +18,18 @@ export const updateLogs = async (req, res) => {
 
     if (log) {
       log.pathnames.push(pathname);
+      log.count = log.count + 1;
       await log.save();
       return res.status(200).json({ message: "Log updated successfully" });
     }
 
-    const newDoc = new logs({ projectID, filter, pathnames: [pathname] });
-    await newDoc.save();
+    const newLogDoc = new logs({
+      projectID,
+      filter,
+      pathnames: [pathname],
+      count: 1,
+    });
+    await newLogDoc.save();
     return res.status(200).json({ message: "Log created successfully" });
   } catch (error) {
     console.log("Error updating logs: ", error.message);
--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import logs from "../models/logs.js";
 
 export const isAuth = async (req, res, next) => {
   try {
@@ -145,57 +146,107 @@ export const getUser = async (req, res) => {
   }
 };
 
-// NOT IMPLEMENTED
-export const getCount = async (req, res) => {
+// FETCH LOGS FROM DATABASE
+export const getLogs = async (req, res) => {
   try {
-    const backendURL = process.env.MAIN_SERVER_URL;
-    const { filters } = req.body;  // Assuming filters are sent in the request body
+    const { projectID, filter, limit, skip, startDate, endDate } = req.body;
 
-    const backendResponse = await axios.post(backendURL + "/data/get-count", { filters }, {
-      headers: {
-        Authorization: req.headers.authorization,
-        Cookie: req.headers.cookie,
-      },
+    switch (true) {
+      case !projectID:
+        return res.status(400).json({ message: "PROJECT ID NOT PROVIDED" });
+      case !startDate || !endDate:
+        return res.status(400).json({ message: "DATE RANGE NOT PROVIDED" });
+      case !filter:
+        return res.status(400).json({ message: "FILTER NOT PROVIDED" });
+    }
+
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+    endDateObj.setHours(23, 59, 59, 999);
+
+    const logsData = await logs.find({
+      projectID,
+      createdAt: { $gte: startDateObj, $lte: endDateObj },
     });
 
-    // Forward only necessary headers
-    const headersToForward = ["content-type", "authorization", "set-cookie"];
-    headersToForward.forEach((header) => {
-      if (backendResponse.headers[header]) {
-        res.setHeader(header, backendResponse.headers[header]);
+    if (logsData.length === 0) {
+      return res.status(404).json({ message: "NO LOGS FOUND" });
+    }
+
+    return res.status(200).json({ data: logsData });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCount = async (req, res) => {
+  try {
+    const { projectID, filter, startDate, endDate } = req.body;
+
+    switch (true) {
+      case !projectID:
+        return res.status(400).json({ message: "PROJECT ID NOT PROVIDED" });
+      case !filter:
+        return res.status(400).json({ message: "FILTER NOT PROVIDED" });
+    }
+
+    let searchFilter = {};
+    for (const key in filter) {
+      searchFilter[key] = filter[key].split(", ");
+    }
+
+    let filterConditions = Object.entries(searchFilter).reduce(
+      (acc, [key, value]) => {
+        if (Array.isArray(value)) {
+          return acc.flatMap((obj) =>
+            value.map((val) => ({ ...obj, [key]: val }))
+          );
+        } else {
+          return acc.map((obj) => ({ ...obj, [key]: value }));
+        }
+      },
+      [{}]
+    );
+
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+    endDateObj.setHours(23, 59, 59, 999);
+
+    const pipeline = [
+      {
+        $match: {
+          projectID,
+          createdAt: { $gte: startDateObj, $lte: endDateObj },
+          $or: filterConditions.map((condition) => ({ filter: condition })),
+        },
+      },
+      {
+        $group: {
+          _id: "$filter",
+          count: { $sum: "$count" },
+        },
+      },
+    ];
+
+    const resObj = await logs.aggregate(pipeline).exec();
+
+    const COUNT = {};
+    resObj.forEach(({ _id, count }) => {
+      for (const key in _id) {
+        if (!COUNT[key]) {
+          COUNT[key] = {};
+        }
+        if (!COUNT[key][_id[key]]) {
+          COUNT[key][_id[key]] = 0;
+        }
+        COUNT[key][_id[key]] += count;
       }
     });
 
-    // Forward the status and data from backendResponse to the client
-    return res.status(backendResponse.status).send(backendResponse.data);
+    return res.status(200).json({ message: "SUCCESS", data: COUNT });
   } catch (error) {
-    if (error.response) {
-      return res.status(error.response.status).json(error.response.data);
-    } else {
-      return res.status(500).json({ message: "INTERNAL SERVER ERROR" });
-    }
-  }
-};
-export const updateCount = async (req, res) => {
-  try {
-    return res.status(200).json({ message: "NOT IMPLEMENTED" });
-  } catch (error) {
-    return res.status(500).json({ message: "INTERNAL SERVER ERROR" });
-  }
-};
-
-export const getLogs = async (req, res) => {
-  try {
-    return res.status(200).json({ message: "NOT IMPLEMENTED" });
-  } catch (error) {
-    return res.status(500).json({ message: "INTERNAL SERVER ERROR" });
-  }
-};
-
-export const updateLogs = async (req, res) => {
-  try {
-    return res.status(200).json({ message: "NOT IMPLEMENTED" });
-  } catch (error) {
-    return res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
   }
 };
